1600866e9a055df1e509640b60da7fa5479a5f37
NathanAbrahamSinaga <nathansinaga2002@gmail.com>
2024-03-01 21:33:56 +0700

    ten commit

---
+++ frontend/src/components/WorkoutForm.js
@@ -0,0 +1,9 @@
+const WorkoutForm = () => {
+    const [title, setTitle] = useState('')
+    const [load, setLoad] = useState('')
+    const [reps, setReps] = useState('')
+    
+    return (
+
+    )
+}
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
   return (
     <div className="home">
       <div className="workouts">
-        {workouts && workouts.map(workout => (
+        {workouts && workouts.map((workout) => (
           <WorkoutDetails workout={workout} key={workout._id} />
         ))}
       </div>
