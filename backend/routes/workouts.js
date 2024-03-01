c8fbf167807a450c09318b6f8601eae93f2e4b95
NathanAbrahamSinaga <nathansinaga2002@gmail.com>
2024-03-01 17:27:27 +0700

    third commit

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -2,19 +2,29 @@ const express = require('express')
 
 const router = express.Router()
 
-// get all workouts
+// GET all workouts
 router.get('/', (req, res) => {
-    res.json({message: 'Hello World'})
+  res.json({mssg: 'GET all workouts'})
 })
 
-// get a single workout
+// GET a single workout
 router.get('/:id', (req, res) => {
-    res.json({message: `GET a single workout`})
+  res.json({mssg: 'GET a single workout'})
 })
 
-// post a new workout
+// POST a new workout
 router.post('/', (req, res) => {
-    res.json({mssg: 'POST a new workout'})
+  res.json({mssg: 'POST a new workout'})
+})
+
+// DELETE a workout
+router.delete('/:id', (req, res) => {
+  res.json({mssg: 'DELETE a workout'})
+})
+
+// UPDATE a workout
+router.patch('/:id', (req, res) => {
+  res.json({mssg: 'UPDATE a workout'})
 })
 
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,32 @@
-require("dotenv").config()
+require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 // express app
 const app = express()
 
-//middleware
+// middleware
+app.use(express.json())
+
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
+  console.log(req.path, req.method)
+  next()
 })
 
-//routes
-app.use('/api/workouts',workoutRoutes)
+// routes
+app.use('/api/workouts', workoutRoutes)
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening for requests http://localhost:4000')
-})
-
-process.env
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
