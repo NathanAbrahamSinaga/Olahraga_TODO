ead0ed5ed8d55cf24bd6ab55aa6173175c9b43f2
NathanAbrahamSinaga <nathansinaga2002@gmail.com>
2024-03-02 12:34:19 +0700

    ten commit

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 
 // components
 import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
   const [workouts, setWorkouts] = useState(null)
@@ -22,10 +23,11 @@ const Home = () => {
   return (
     <div className="home">
       <div className="workouts">
-        {workouts && workouts.map((workout) => (
+        {workouts && workouts.map(workout => (
           <WorkoutDetails workout={workout} key={workout._id} />
         ))}
       </div>
+      <WorkoutForm />
     </div>
   )
 }
